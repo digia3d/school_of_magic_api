4e656d3215f65b8f7d6f3d67c63fbcc7082a24c2
Aleksandra Ujvari <ujvari65@gmail.com>
2023-03-27 19:03:25 +0200

    Fix linters errors

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,18 +3,18 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_or_create_by(name: params[:name])
     if user.persisted?
       session[:user_id] = user.id # Set the session variable
-      render json: {message: "Welcome, #{user.name}!"}, status: :ok
+      render json: { message: "Welcome, #{user.name}!" }, status: :ok
     else
-      render json: {error: user.errors.full_messages.join(", ")}, status: :unprocessable_entity
+      render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
     end
   end
 
   def show
     if session[:user_id]
       user = User.find(session[:user_id])
-      render json: {user_id: user.id, user_name: user.name}, status: :ok
+      render json: { user_id: user.id, user_name: user.name }, status: :ok
     else
-      render json: {error: "You are not logged in"}, status: :unauthorized
+      render json: { error: 'You are not logged in' }, status: :unauthorized
     end
   end
 
@@ -22,9 +22,9 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user
       session[:user_id] = user.id # Set the session variable
-      render json: {message: "Logged in successfully as #{user.name}"}, status: :ok
+      render json: { message: "Logged in successfully as #{user.name}" }, status: :ok
     else
-      render json: {error: "Invalid name"}, status: :unauthorized
+      render json: { error: 'Invalid name' }, status: :unauthorized
     end
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def index
     users = User.all
-    render json: { users: users }, status: :ok
+    render json: { users: }, status: :ok
   end
 
   def create
@@ -17,9 +17,9 @@ class Api::V1::UsersController < ApplicationController
   def show
     if session[:user_id]
       user = User.find(session[:user_id])
-      render json: {user_id: user.id, user_name: user.name}, status: :ok
+      render json: { user_id: user.id, user_name: user.name }, status: :ok
     else
-      render json: {error: "You are not logged in"}, status: :unauthorized
+      render json: { error: 'You are not logged in' }, status: :unauthorized
     end
   end
 
@@ -29,7 +29,7 @@ class Api::V1::UsersController < ApplicationController
       session[:user_id] = user.id
       render json: { message: "Logged in successfully as #{user.name}" }, status: :ok
     else
-      render json: { error: "Invalid name" }, status: :unauthorized
+      render json: { error: 'Invalid name' }, status: :unauthorized
     end
   end
 end
