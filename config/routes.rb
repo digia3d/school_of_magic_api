dfef803ff5f35fc8c6c437baa77b9008321bf3de
Aleksandra Ujvari <ujvari65@gmail.com>
2023-03-27 17:41:57 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :houses, only: [:index, :show] do
+        resources :characters, only: [:index]
+      end
+      resources :characters, only: [:index, :show] do
+        member do
+          get :houses
+        end
+      end
+    end
+  end
 end
