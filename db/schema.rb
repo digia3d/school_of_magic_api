9d84506a703145d0b8dd2a5bbcbcdda0273a168e
Aleksandra Ujvari <ujvari65@gmail.com>
2023-03-27 18:27:13 +0200

    Create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_135129) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_162522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_135129) do
   end
 
   create_table "houses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
