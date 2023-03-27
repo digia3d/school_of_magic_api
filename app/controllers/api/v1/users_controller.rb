d1538268fc8c1b0e40995ad0ea74f42254cbddaa
Aleksandra Ujvari <ujvari65@gmail.com>
2023-03-27 18:42:52 +0200

    Add actions for users controlers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: { users: users }, status: :ok
+  end
+
+  def create
+    user = User.new(name: params[:name])
+
+    if user.save
+      render json: { message: "Welcome, #{user.name}!" }, status: :created
+    else
+      render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    if session[:user_id]
+      user = User.find(session[:user_id])
+      render json: {user_id: user.id, user_name: user.name}, status: :ok
+    else
+      render json: {error: "You are not logged in"}, status: :unauthorized
+    end
+  end
+
+  def login
+    user = User.find_by(name: params[:name])
+    if user.present?
+      session[:user_id] = user.id
+      render json: { message: "Logged in successfully as #{user.name}" }, status: :ok
+    else
+      render json: { error: "Invalid name" }, status: :unauthorized
+    end
+  end
+
+  
 end
