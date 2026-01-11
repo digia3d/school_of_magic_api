dfa788478b75e7622b855be89d5ec4d142409ccc
Aleksandra <ujvari65@gmail.com>
2026-01-11 17:01:47 +0100

    Fix Render build: force Nokogiri system libraries

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+echo "🌙 Setting Nokogiri to use system libraries..."
+bundle config set --local build.nokogiri --use-system-libraries
+bundle config set --local force_ruby_platform true
+
+echo "🌱 Installing gems..."
 bundle install
+
 bundle exec rake db:migrate
 bundle exec rake db:seed
