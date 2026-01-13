b89bff2b8611a234ff6370da61bf8ed6091fbd24
Aleksandra Ujvari <ujvari65@gmail.com>
2026-01-13 19:43:30 +0100

    Refactor API routes for clarity and organization

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,23 @@
 Rails.application.routes.draw do
   root to: proc { [200, {}, ['School of Magic API is running 🪄']] }
-    
+
   namespace :api do
     namespace :v1 do
-      # Users routes
+      # Users
       resources :users, only: [:create, :index]
 
-      # Login routes
+      # Login
       post '/login', to: 'sessions#create'
 
-      # Houses routes
+      # Houses
       resources :houses, only: [:index, :show] do
-        # Custom route to get characters by house name
-        # Example: GET /api/v1/houses/Gryffindor/characters
+        # GET /api/v1/houses/:house_id/characters
         get 'characters', to: 'characters#by_house'
       end
 
-      # Characters routes
-      resources :characters, only: [:index, :show, :create] do
-        # Optional route to see which houses a character belongs to
-        end
-      end
+      # Characters
+      resources :characters, only: [:index, :show, :create]
     end
   end
 end
+
