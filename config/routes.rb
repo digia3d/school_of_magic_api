c9a8e6eb61ea2f8509ebebec3c89e45f3164d850
Aleksandra Ujvari <ujvari65@gmail.com>
2026-01-13 19:35:34 +0100

    Refactor routes for sessions and houses

--- config/routes.rb
+++ config/routes.rb
@@ -8,20 +8,17 @@ Rails.application.routes.draw do
 
       # Login routes
       post '/login', to: 'sessions#create'
-      post '/sessions', to: 'sessions#create'
 
       # Houses routes
       resources :houses, only: [:index, :show] do
         # Custom route to get characters by house name
         # Example: GET /api/v1/houses/Gryffindor/characters
-        get ':house_name/characters', to: 'characters#by_house', on: :collection
+        get 'characters', to: 'characters#by_house'
       end
 
       # Characters routes
       resources :characters, only: [:index, :show, :create] do
         # Optional route to see which houses a character belongs to
-        member do
-          get :houses
         end
       end
     end
