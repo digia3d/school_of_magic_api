ffbc3d4e4051b4ec28e275fdea9d65c105a0c7e8
Aleksandra Ujvari <ujvari65@gmail.com>
2026-01-13 19:37:42 +0100

    Refactor by_house method to use house_id

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -24,18 +24,27 @@ class Api::V1::CharactersController < ApplicationController
   end
 
   # GET /api/v1/houses/:house_name/characters
-  def by_house
-    house_name = params[:house_name] # ovo je važno
-    house = House.find_by(name: house_name)
+def by_house
+  house = House.find_by(id: params[:house_id])
 
-    if house.nil?
-      render json: { error: 'House not found' }, status: :not_found
-      return
-    end
+  return render json: { error: 'House not found' }, status: :not_found if house.nil?
 
-    characters = Character.where(house: house)
-    render json: characters_as_json(characters)
-  end
+  characters = Character.where(house_id: house.id)
+
+  render json: characters.map { |char|
+    char.as_json(
+      only: %i[
+        id name species gender dateOfBirth yearOfBirth ancestry
+        eyeColour hairColour patronus actor alive
+      ],
+      include: {
+        wand: { only: %i[wood core length] },
+        house: { only: [:name] }
+      }
+    ).merge(image_url: char.image_url)
+  }
+end
+
 
   private
 
