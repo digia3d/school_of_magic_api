ccd49d13cbc0c79c877047a43bcdc92b6a4de6d9
Aleksandra <ujvari65@gmail.com>
2026-01-15 14:18:47 +0100

    Fix rubocop errors

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CharactersController < ApplicationController
-  before_action :set_character, only: %i[show houses]
+  before_action :set_character, only: %i[show]
 
   # GET /api/v1/characters
   def index
@@ -43,27 +43,26 @@ class Api::V1::CharactersController < ApplicationController
   end
 
   # GET /api/v1/houses/:house_name/characters
-def by_house
-  house = House.find_by(id: params[:house_id])
+  def by_house
+    house = House.find_by(id: params[:house_id])
 
-  return render json: { error: 'House not found' }, status: :not_found if house.nil?
+    return render json: { error: 'House not found' }, status: :not_found if house.nil?
 
-  characters = Character.where(house_id: house.id)
+    characters = Character.where(house_id: house.id)
 
-  render json: characters.map { |char|
-    char.as_json(
-      only: %i[
-        id name species gender dateOfBirth yearOfBirth ancestry
-        eyeColour hairColour patronus actor alive
-      ],
-      include: {
-        wand: { only: %i[wood core length] },
-        house: { only: [:name] }
-      }
-    ).merge(image_url: char.image_url)
-  }
-end
-
+    render json: characters.map { |char|
+      char.as_json(
+        only: %i[
+          id name species gender dateOfBirth yearOfBirth ancestry
+          eyeColour hairColour patronus actor alive
+        ],
+        include: {
+          wand: { only: %i[wood core length] },
+          house: { only: [:name] }
+        }
+      ).merge(image_url: char.image_url)
+    }
+  end
 
   private
 
